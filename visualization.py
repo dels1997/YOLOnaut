d59178c650fe2e957850bfd8cba276255f550d33
Karlo Delić <karlodelic1997@gmail.com>
2024-01-19 16:13:23 +0100

    Iterate over the valid detections and draw bounding boxes and class names, along with label cards as labesl

--- visualization.py
+++ visualization.py
@@ -13,6 +13,15 @@ FONT_SIZE_RATIO_OF_IMAGE_WIDTH = config.FONT_SIZE_RATIO_OF_IMAGE_WIDTH
 
 font_path = config.LABEL_FONT_PATH
 
+def get_contrast_text_color(background_color):
+    luminance = \
+        0.299 * background_color[0] + \
+        0.587 * background_color[1] + \
+        0.114 * background_color[2]
+    luminance /= 256
+    text_color = 'dimgray' if luminance > 0.5 else 'whitesmoke'
+    return text_color
+
 def visualize_single_prediction_tensor_notation(
         image_path, target_path, S=S, C=C,THRESHOLD=THRESHOLD,
         color_map_name='hsv_r', font_path=font_path
@@ -79,3 +88,49 @@ def visualize_single_prediction_tensor_notation(
     ]
 
     draw = ImageDraw.Draw(im)
+
+    # Iterate over the valid detections and draw bounding boxes and class names.
+    for i in range(num_valid_detections):
+        _class_index = int(class_index[i].item())
+        _class_name = CLASSES_DICT_INV[_class_index]
+        color = color_map(_class_index % C)
+        color = tuple([int(c * 255.999) for c in color])
+
+        # Select dimensions of the current bounding box.
+        _x1, _y1, _x2, _y2 = \
+            x1[i].item(), y1[i].item(), x2[i].item(), y2[i].item()
+
+        # Draw a bounding box of prediction.
+        draw.rectangle([_x1, _y1, _x2, _y2], outline=color)
+
+        # Draw class label card as background for text, with padding for
+        # class name text.
+        _left, _top, _right, _bottom = \
+            draw.textbbox((_x1 + PAD, _y1 + PAD), _class_name, font=font)
+        _text_height = _bottom - _top
+        _text_top = _y1 - 1.5 * _text_height
+        _label_card_rectangle_top = _top - 1.5 * _text_height - 2 * PAD
+        _label_card_rectangle_bottom = _bottom - 1.5 * _text_height
+
+        # If the label card is out of image bounds, then shift it to the
+        # inside of the bounding box.
+        if _label_card_rectangle_top < 0:
+            _label_card_rectangle_top = _top - 0.5 * _text_height
+            _label_card_rectangle_bottom = _bottom + PAD
+            _text_top = _y1 + 0.5 * _text_height - PAD
+
+        # Draw label card background.
+        draw.rectangle(
+            [_left - PAD, _label_card_rectangle_top,
+             _right + PAD, _label_card_rectangle_bottom],
+            fill=color
+        )
+
+        # Draw class name text.
+        text_color = get_contrast_text_color(color)
+        draw.text(
+            (_x1 + PAD, _text_top), _class_name, font=font,
+            fill=text_color
+        )
+
+    im.show()
